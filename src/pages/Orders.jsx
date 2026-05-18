80a7f70328e43caab3d160e17d67513e1f78c8d9
Luiz Paulo <seuemail@gmail.com>
2026-05-18 20:52:28 -0300

    test: isolate orders route

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
+import { ordersService } from '../services/orders';
+
 export default function Orders() {
+  useEffect(() => {
+    async function test() {
+      try {
+        const data =
+          await ordersService.getOrders();
+
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    test();
+  }, []);
+
   return (
     <div className="p-10 text-white text-5xl">
-      PEDIDOS FUNCIONANDO
+      TESTE ORDERS SERVICE
     </div>
   );
 }
