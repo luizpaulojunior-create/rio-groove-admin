9964ecad19a0f53af8ad850a4d6bdd5a28ba1010
Luiz Paulo <seuemail@gmail.com>
2026-05-18 22:22:16 -0300

    temp: simplify AdminLayout to test production error

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,24 +1,11 @@
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import Header from '../components/Header';
 
 export default function AdminLayout() {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
   return (
-    <div className="flex min-h-screen bg-[var(--color-background)]">
-      {/* Sidebar for Desktop & Mobile Drawer */}
-      <Sidebar isOpen={isMobileMenuOpen} setIsOpen={setIsMobileMenuOpen} />
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1>ADMIN LAYOUT TEST</h1>
 
-      {/* Main Content Area */}
-      <div className="flex-1 flex flex-col min-w-0 lg:ml-[320px]">
-        <Header onMenuClick={() => setIsMobileMenuOpen(true)} />
-        
-        <main className="flex-1 p-4 md:p-8 overflow-x-hidden">
-          <Outlet />
-        </main>
-      </div>
+      <Outlet />
     </div>
   );
 }
